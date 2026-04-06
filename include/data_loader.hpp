29e2c0c043a5213efc111154ddde64d7e662b27a
unknown <1nathankong@gmail.com>
2026-04-06 11:48:39 -0400

    reformat file to follow column rule

--- include/data_loader.hpp
+++ include/data_loader.hpp
@@ -12,5 +12,6 @@ struct Dataset {
 
 void parse_labels(std::ifstream& file);
 void parse_images(std::ifstream& file);
-void define_dataset(std::ifstream& img_file, std::ifstream&lbl_file, Dataset& data);
+void define_dataset(std::ifstream& img_file, 
+    std::ifstream&lbl_file, Dataset& data);
 void load(const std::string& path, Dataset& data);
